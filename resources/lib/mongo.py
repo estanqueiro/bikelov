fe0c48ce3b04c38d59a15c6d2cf02501be432116
pauloestanqueiro <paulo.estanqueiro@gmail.com>
2020-06-25 11:39:49 +0100

    Includes 'remove_bike' before 'create_bike_ad' in mongo.py to clean up database

--- resources/lib/mongo.py
+++ resources/lib/mongo.py
@@ -16,6 +16,7 @@ def remove_bike(name):
     bikes().delete_many({'name': name})
 
 def create_bike_ad(ad, user_id):
+    remove_bike(ad['name'])
     ad["user"] = bson.objectid.ObjectId(user_id)
     return bikes().insert_one(ad).inserted_id
 
